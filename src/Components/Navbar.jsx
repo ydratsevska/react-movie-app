a7a1b5fd7efad55eb392bef7880e417018740982
ydratsevska <y.dratsevska@gmail.com>
2025-11-19 19:22:47 +0200

    add navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Navbar() {
-return <div>Navbar</div>
+return (
+    
+
+<div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex gap-2">
+    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+  </div>
+</div>
+)
 }
 
 
