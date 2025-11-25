d8108ceacf78f205f6900c1c48ea5c90bdadb57d
ydratsevska <y.dratsevska@gmail.com>
2025-11-25 23:43:09 +0200

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@ import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [err, setErr] = useState('')
+
+  const API_KEY = import.meta.env.VITE_OMDb_API_KEY;
+  useEffect(
+    const fetchMovies = async() => {
+        try {
+          let url;
+
+          url = `http://www.omdbapi.com/?apikey=${API_KEY}&`
+        } catch {
+
+        }
+  }
+  )
+
+  
 
   return (
     <>
